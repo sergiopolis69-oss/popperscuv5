5742455f014e0d583b733ee9def11c729398c5f3
sergiopolis69-oss <sergiopolis69@gmail.com>
2025-09-28 19:34:21 -0600

    Actualizar xml_backup.dart

--- lib/utils/xml_backup.dart
+++ lib/utils/xml_backup.dart
@@ -1,153 +1,150 @@
 import 'dart:io';
 import 'package:xml/xml.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:file_saver/file_saver.dart';
 import 'package:sqflite/sqflite.dart';
 import '../data/database.dart';
-import 'package:path_provider/path_provider.dart';
 
-Future<Directory> _downloads() async {
-  final dir = await getExternalStorageDirectory();
-  return dir ?? await getApplicationDocumentsDirectory();
-}
+// ============ EXPORTS ============
 
-Future<File> exportClientsXml() async {
+Future<void> exportClientsXml() async {
   final db = await DatabaseHelper.instance.db;
   final rows = await db.query('customers');
-  final builder = XmlBuilder()..processing('xml', 'version="1.0"');
-  builder.element('customers', nest: (){
+  final builder = XmlBuilder();
+  builder.processing('xml', 'version="1.0"');
+  builder.element('customers', nest: () {
     for (final r in rows) {
-      builder.element('customer', nest: (){
-        builder.element('phone', nest: r['phone'] ?? '');
-        builder.element('name', nest: r['name'] ?? '');
-        builder.element('address', nest: r['address'] ?? '');
+      builder.element('customer', nest: () {
+        builder.element('phone', nest: r['phone']);
+        builder.element('name', nest: r['name']);
+        builder.element('address', nest: r['address']);
       });
     }
   });
-  final file = File('${(await _downloads()).path}/clientes.xml');
-  return file.writeAsString(builder.buildDocument().toXmlString(pretty: true));
+  final xml = builder.buildDocument().toXmlString(pretty: true);
+  final dir = await getDownloadsDirectory();
+  final file = File('${dir!.path}/clientes.xml');
+  await file.writeAsString(xml);
+  await FileSaver.instance.saveFile(name: 'clientes', bytes: xml.codeUnits, ext: 'xml', mimeType: MimeType.text);
 }
 
-Future<File> exportProductsXml() async {
+Future<void> exportProductsXml() async {
   final db = await DatabaseHelper.instance.db;
   final rows = await db.query('products');
-  final b = XmlBuilder()..processing('xml', 'version="1.0"');
-  b.element('products', nest: (){
+  final builder = XmlBuilder();
+  builder.processing('xml', 'version="1.0"');
+  builder.element('products', nest: () {
     for (final r in rows) {
-      b.element('product', nest: (){
-        b.element('id', nest: r['id']?.toString() ?? '');
-        b.element('sku', nest: r['sku'] ?? '');
-        b.element('name', nest: r['name'] ?? '');
-        b.element('category', nest: r['category'] ?? '');
-        b.element('stock', nest: r['stock']?.toString() ?? '0');
-        b.element('last_purchase_price', nest: r['last_purchase_price']?.toString() ?? '0');
-        b.element('last_purchase_date', nest: r['last_purchase_date'] ?? '');
+      builder.element('product', nest: () {
+        builder.element('id', nest: r['id']);
+        builder.element('sku', nest: r['sku']);
+        builder.element('name', nest: r['name']);
+        builder.element('category', nest: r['category']);
+        builder.element('stock', nest: r['stock']);
+        builder.element('last_purchase_price', nest: r['last_purchase_price']);
+        builder.element('last_purchase_date', nest: r['last_purchase_date']);
+        builder.element('default_sale_price', nest: r['default_sale_price']);
+        builder.element('initial_cost', nest: r['initial_cost']);
       });
     }
   });
-  final file = File('${(await _downloads()).path}/productos.xml');
-  return file.writeAsString(b.buildDocument().toXmlString(pretty: true));
+  final xml = builder.buildDocument().toXmlString(pretty: true);
+  final dir = await getDownloadsDirectory();
+  final file = File('${dir!.path}/productos.xml');
+  await file.writeAsString(xml);
+  await FileSaver.instance.saveFile(name: 'productos', bytes: xml.codeUnits, ext: 'xml', mimeType: MimeType.text);
 }
 
-Future<File> exportSuppliersXml() async {
+Future<void> exportSuppliersXml() async {
   final db = await DatabaseHelper.instance.db;
   final rows = await db.query('suppliers');
-  final b = XmlBuilder()..processing('xml', 'version="1.0"');
-  b.element('suppliers', nest: (){
+  final builder = XmlBuilder();
+  builder.processing('xml', 'version="1.0"');
+  builder.element('suppliers', nest: () {
     for (final r in rows) {
-      b.element('supplier', nest: (){
-        b.element('id', nest: r['id']?.toString() ?? '');
-        b.element('name', nest: r['name'] ?? '');
-        b.element('phone', nest: r['phone'] ?? '');
-        b.element('address', nest: r['address'] ?? '');
+      builder.element('supplier', nest: () {
+        builder.element('id', nest: r['id']);
+        builder.element('name', nest: r['name']);
+        builder.element('phone', nest: r['phone']);
+        builder.element('address', nest: r['address']);
       });
     }
   });
-  final file = File('${(await _downloads()).path}/proveedores.xml');
-  return file.writeAsString(b.buildDocument().toXmlString(pretty: true));
+  final xml = builder.buildDocument().toXmlString(pretty: true);
+  final dir = await getDownloadsDirectory();
+  final file = File('${dir!.path}/proveedores.xml');
+  await file.writeAsString(xml);
+  await FileSaver.instance.saveFile(name: 'proveedores', bytes: xml.codeUnits, ext: 'xml', mimeType: MimeType.text);
 }
 
-Future<File> exportSalesXml() async {
+Future<void> exportSalesXml() async {
   final db = await DatabaseHelper.instance.db;
-  final sales = await db.query('sales');
+  final rows = await db.query('sales');
   final items = await db.query('sale_items');
-  final itemsBySale = <int, List<Map<String, Object?>>>{};
-  for (final it in items) {
-    final sid = it['sale_id'] as int;
-    itemsBySale.putIfAbsent(sid, ()=>[]).add(it);
-  }
-  final b = XmlBuilder()..processing('xml', 'version="1.0"');
-  b.element('sales', nest: (){
-    for (final s in sales) {
-      b.element('sale', nest: (){
-        b.element('id', nest: s['id'].toString());
-        b.element('customer_phone', nest: s['customer_phone'] ?? '');
-        b.element('payment_method', nest: s['payment_method'] ?? '');
-        b.element('place', nest: s['place'] ?? '');
-        b.element('shipping_cost', nest: s['shipping_cost']?.toString() ?? '0');
-        b.element('discount', nest: s['discount']?.toString() ?? '0');
-        b.element('date', nest: s['date'] ?? '');
-        b.element('items', nest: (){
-          for (final it in itemsBySale[s['id']] ?? const []) {
-            b.element('item', nest: (){
-              b.element('product_id', nest: it['product_id'].toString());
-              b.element('quantity', nest: it['quantity'].toString());
-              b.element('unit_price', nest: it['unit_price'].toString());
+  final builder = XmlBuilder();
+  builder.processing('xml', 'version="1.0"');
+  builder.element('sales', nest: () {
+    for (final r in rows) {
+      builder.element('sale', nest: () {
+        builder.element('id', nest: r['id']);
+        builder.element('customer_phone', nest: r['customer_phone']);
+        builder.element('payment_method', nest: r['payment_method']);
+        builder.element('place', nest: r['place']);
+        builder.element('shipping_cost', nest: r['shipping_cost']);
+        builder.element('discount', nest: r['discount']);
+        builder.element('date', nest: r['date']);
+        builder.element('items', nest: () {
+          for (final it in items.where((e) => e['sale_id'] == r['id'])) {
+            builder.element('item', nest: () {
+              builder.element('product_id', nest: it['product_id']);
+              builder.element('quantity', nest: it['quantity']);
+              builder.element('unit_price', nest: it['unit_price']);
             });
           }
         });
       });
     }
   });
-  final file = File('${(await _downloads()).path}/ventas.xml');
-  return file.writeAsString(b.buildDocument().toXmlString(pretty: true));
+  final xml = builder.buildDocument().toXmlString(pretty: true);
+  final dir = await getDownloadsDirectory();
+  final file = File('${dir!.path}/ventas.xml');
+  await file.writeAsString(xml);
+  await FileSaver.instance.saveFile(name: 'ventas', bytes: xml.codeUnits, ext: 'xml', mimeType: MimeType.text);
 }
 
-Future<File> exportPurchasesXml() async {
+Future<void> exportPurchasesXml() async {
   final db = await DatabaseHelper.instance.db;
-  final purchases = await db.query('purchases');
+  final rows = await db.query('purchases');
   final items = await db.query('purchase_items');
-  final itemsBy = <int, List<Map<String,Object?>>>{};
-  for (final it in items) {
-    final pid = it['purchase_id'] as int;
-    itemsBy.putIfAbsent(pid, ()=>[]).add(it);
-  }
-  final b = XmlBuilder()..processing('xml', 'version="1.0"');
-  b.element('purchases', nest: (){
-    for (final p in purchases) {
-      b.element('purchase', nest: (){
-        b.element('id', nest: p['id'].toString());
-        b.element('folio', nest: p['folio'] ?? '');
-        b.element('supplier_id', nest: p['supplier_id'].toString());
-        b.element('date', nest: p['date'] ?? '');
-        b.element('items', nest: (){
-          for (final it in itemsBy[p['id']] ?? const []) {
-            b.element('item', nest: (){
-              b.element('product_id', nest: it['product_id'].toString());
-              b.element('quantity', nest: it['quantity'].toString());
-              b.element('unit_cost', nest: it['unit_cost'].toString());
+  final builder = XmlBuilder();
+  builder.processing('xml', 'version="1.0"');
+  builder.element('purchases', nest: () {
+    for (final r in rows) {
+      builder.element('purchase', nest: () {
+        builder.element('id', nest: r['id']);
+        builder.element('folio', nest: r['folio']);
+        builder.element('supplier_id', nest: r['supplier_id']);
+        builder.element('date', nest: r['date']);
+        builder.element('items', nest: () {
+          for (final it in items.where((e) => e['purchase_id'] == r['id'])) {
+            builder.element('item', nest: () {
+              builder.element('product_id', nest: it['product_id']);
+              builder.element('quantity', nest: it['quantity']);
+              builder.element('unit_cost', nest: it['unit_cost']);
             });
           }
         });
       });
     }
   });
-  final file = File('${(await _downloads()).path}/compras.xml');
-  return file.writeAsString(b.buildDocument().toXmlString(pretty: true));
+  final xml = builder.buildDocument().toXmlString(pretty: true);
+  final dir = await getDownloadsDirectory();
+  final file = File('${dir!.path}/compras.xml');
+  await file.writeAsString(xml);
+  await FileSaver.instance.saveFile(name: 'compras', bytes: xml.codeUnits, ext: 'xml', mimeType: MimeType.text);
 }
 
-Future<void> importSuppliersXml(File file) async {
-  final db = await DatabaseHelper.instance.db;
-  final doc = XmlDocument.parse(await file.readAsString());
-  final batch = db.batch();
-  for (final x in doc.findAllElements('supplier')) {
-    final name = x.getElement('name')?.innerText ?? '';
-    final phone = x.getElement('phone')?.innerText ?? '';
-    final address = x.getElement('address')?.innerText ?? '';
-    if (name.isEmpty) continue;
-    batch.insert('suppliers', {'name': name, 'phone': phone, 'address': address},
-      conflictAlgorithm: ConflictAlgorithm.replace);
-  }
-  await batch.commit(noResult: true);
-}
+// ============ IMPORTS ============
 
 Future<void> importClientsXml(File file) async {
   final db = await DatabaseHelper.instance.db;
@@ -170,18 +167,35 @@ Future<void> importProductsXml(File file) async {
   final doc = XmlDocument.parse(await file.readAsString());
   final batch = db.batch();
   for (final p in doc.findAllElements('product')) {
-    final sku = p.getElement('sku')?.innerText ?? '';
     final name = p.getElement('name')?.innerText ?? '';
     if (name.isEmpty) continue;
-    final map = {
-      'sku': sku.isEmpty ? null : sku,
+    batch.insert('products', {
+      'sku': p.getElement('sku')?.innerText,
       'name': name,
       'category': p.getElement('category')?.innerText ?? '',
       'stock': int.tryParse(p.getElement('stock')?.innerText ?? '0') ?? 0,
       'last_purchase_price': double.tryParse(p.getElement('last_purchase_price')?.innerText ?? '0') ?? 0.0,
       'last_purchase_date': p.getElement('last_purchase_date')?.innerText,
-    };
-    batch.insert('products', map, conflictAlgorithm: ConflictAlgorithm.replace);
+      'default_sale_price': double.tryParse(p.getElement('default_sale_price')?.innerText ?? '0') ?? 0.0,
+      'initial_cost': double.tryParse(p.getElement('initial_cost')?.innerText ?? '0') ?? 0.0,
+    }, conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+  await batch.commit(noResult: true);
+}
+
+Future<void> importSuppliersXml(File file) async {
+  final db = await DatabaseHelper.instance.db;
+  final doc = XmlDocument.parse(await file.readAsString());
+  final batch = db.batch();
+  for (final x in doc.findAllElements('supplier')) {
+    final name = x.getElement('name')?.innerText ?? '';
+    if (name.isEmpty) continue;
+    batch.insert('suppliers', {
+      'id': int.tryParse(x.getElement('id')?.innerText ?? ''),
+      'name': name,
+      'phone': x.getElement('phone')?.innerText ?? '',
+      'address': x.getElement('address')?.innerText ?? '',
+    }, conflictAlgorithm: ConflictAlgorithm.replace);
   }
   await batch.commit(noResult: true);
 }
