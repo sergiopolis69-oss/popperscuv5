db68fbaf7811f510a49d13523155ae5f44a8de2d
sergiopolis69-oss <sergiopolis69@gmail.com>
2025-10-10 18:32:31 -0600

    Actualizar sku.dart

--- lib/utils/sku.dart
+++ lib/utils/sku.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
-final _rand = Random.secure();
 String generateSku8() {
   const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-  return List.generate(8, (_) => chars[_rand.nextInt(chars.length)]).join();
+  final rnd = Random.secure();
+  return List.generate(8, (_) => chars[rnd.nextInt(chars.length)]).join();
 }
